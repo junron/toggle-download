941de605a153e69bfb458eb2aeb81c92053b1804
Lam Jun Rong <bobo999bobo@outlook.com>
2019-09-12 16:55:24 +0800

    Format code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,54 +10,54 @@ import java.io.File
 import java.util.*
 
 fun main() {
-    print("Enter toggle URL: ")
-    val reader = Scanner(System.`in`)
-    val url = reader.nextLine()!!.trim()
-    val client = HttpClient()
-    runBlocking {
-        val (m3Url, mediaName, mediaDuration) = getVideoData(url, client)
-        println("Processing video: $mediaName")
-        println("Runtime: $mediaDuration seconds (${secondsToString(mediaDuration)})")
+  print("Enter toggle URL: ")
+  val reader = Scanner(System.`in`)
+  val url = reader.nextLine()!!.trim()
+  val client = HttpClient()
+  runBlocking {
+    val (m3Url, mediaName, mediaDuration) = getVideoData(url, client)
+    println("Processing video: $mediaName")
+    println("Runtime: $mediaDuration seconds (${secondsToString(mediaDuration)})")
 
-        val lines = download(m3Url, client).lines()
-        val resolutions = mutableMapOf<String, String>()
-        var currentKey = ""
-        lines.forEach {
-            if (it.contains("RESOLUTION")) {
-                currentKey = it.substringAfterLast("=")
-            } else if (it.contains("http")) {
-                resolutions[currentKey] = it.substringAfterLast(" ")
-            }
-        }
-        if (resolutions.isEmpty()) {
-            println("No resolutions found.")
-            return@runBlocking
-        }
-        println("Select resolution: ")
-        resolutions.keys.forEachIndexed { i, v ->
-            println("[${i + 1}]: $v")
-        }
-        print("Enter choice [1 to ${resolutions.keys.size}]: ")
-        val resolutionChoice = reader.nextInt()
-        println("You chose: ${resolutions.keys.elementAt(resolutionChoice - 1)}")
-        //        This cannot be null
-        val streamUrl = resolutions[resolutions.keys.elementAt(resolutionChoice - 1)]!!
-        val outputDirectory = File("output-$mediaName")
-        if (!outputDirectory.exists()) {
-            outputDirectory.mkdir()
-        }
-        val tsFiles = mutableListOf<Pair<String,Int>>()
-        var currentVal = -1
-        download(streamUrl, client).lines().forEach {
-            if (it.startsWith("#EXTINF")) {
-//                Time indicator
-                currentVal = it.substringAfter(":").substringBefore(".").toInt()
-            } else if (it.contains("http")) {
-//                Url
-                tsFiles.add(it to currentVal)
-            }
-        }
-        downloadTSFiles(tsFiles, client, outputDirectory)
-        client.close()
+    val lines = download(m3Url, client).lines()
+    val resolutions = mutableMapOf<String, String>()
+    var currentKey = ""
+    lines.forEach {
+      if (it.contains("RESOLUTION")) {
+        currentKey = it.substringAfterLast("=")
+      } else if (it.contains("http")) {
+        resolutions[currentKey] = it.substringAfterLast(" ")
+      }
     }
+    if (resolutions.isEmpty()) {
+      println("No resolutions found.")
+      return@runBlocking
+    }
+    println("Select resolution: ")
+    resolutions.keys.forEachIndexed { i, v ->
+      println("[${i + 1}]: $v")
+    }
+    print("Enter choice [1 to ${resolutions.keys.size}]: ")
+    val resolutionChoice = reader.nextInt()
+    println("You chose: ${resolutions.keys.elementAt(resolutionChoice - 1)}")
+    //        This cannot be null
+    val streamUrl = resolutions[resolutions.keys.elementAt(resolutionChoice - 1)]!!
+    val outputDirectory = File("output-$mediaName")
+    if (!outputDirectory.exists()) {
+      outputDirectory.mkdir()
+    }
+    val tsFiles = mutableListOf<Pair<String, Int>>()
+    var currentVal = -1
+    download(streamUrl, client).lines().forEach {
+      if (it.startsWith("#EXTINF")) {
+        //                Time indicator
+        currentVal = it.substringAfter(":").substringBefore(".").toInt()
+      } else if (it.contains("http")) {
+        //                Url
+        tsFiles.add(it to currentVal)
+      }
+    }
+    downloadTSFiles(tsFiles, client, outputDirectory)
+    client.close()
+  }
 }
--- src/main/kotlin/util/util.kt
+++ src/main/kotlin/util/util.kt
@@ -4,12 +4,12 @@ import java.io.IOException
 import java.time.LocalTime
 
 fun checkFFMPEG(): Boolean {
-    return try {
-        ProcessBuilder().command("ffmpeg", "-version").start().waitFor() == 0
-    } catch (e: IOException) {
-        println(e.message)
-        false
-    }
+  return try {
+    ProcessBuilder().command("ffmpeg", "-version").start().waitFor() == 0
+  } catch (e: IOException) {
+    println(e.message)
+    false
+  }
 }
 
 fun secondsToString(seconds: Int) = LocalTime.ofSecondOfDay(seconds.toLong()).toString()
